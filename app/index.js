eda26ce1ae059c3cf15e9779bf20ed7ab608e503
Madeline Fosmo <maddiefosmo@knights.ucf.edu>
2020-03-25 21:32:11 -0400

    finishing up FF

---
+++ app/index.js
@@ -0,0 +1,49 @@
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    function validateForm() {
+      var isValid = true;
+      $(".form-control").each(function() {
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+
+      $(".chosen-select").each(function() {
+
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+      return isValid;
+    }
+
+    if (validateForm()) {
+      var userData = {
+        name: $("#name").val(),
+        photo: $("#photo").val(),
+        scores: [
+          $("#q1").val(),
+          $("#q2").val(),
+          $("#q3").val(),
+          $("#q4").val(),
+          $("#q5").val(),
+          $("#q6").val(),
+          $("#q7").val(),
+          $("#q8").val(),
+          $("#q9").val(),
+          $("#q10").val()
+        ]
+      };
+
+      $.post("/api/friends", userData, function(data) {
+        $("#match-name").text(data.name);
+        $("#match-img").attr("src", data.photo);
+
+        $("#results-modal").modal("toggle");
+
+      });
+    } else {
+      alert("fields are empty");
+    }
+  });
